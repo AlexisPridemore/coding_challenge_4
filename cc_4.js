c563a89619fa87b2a35040ec9a75797ebe6a4163
Alexis Pridemore <alpridemore@usf.edu>
2025-02-02 16:40:16 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,17 @@
+// Task 1 If Statements, Scenario: Customer Discounts
+
+let purchaseAmount = 500; 
+
+let finalAmount = purchaseAmount;
+
+if (purchaseAmount > 100) {   //Apply a discount if the amount is greater than $100
+    finalAmount = purchaseAmount * 0.9; 
+    console.log("Discount applied")
+};
+console.log('Final amount after discount: $${finalAmount}');
+
+
+
+
+
+
